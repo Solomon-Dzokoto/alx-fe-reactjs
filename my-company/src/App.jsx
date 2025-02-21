273eda6062ec53a12bba964665158d6b06f69d9d
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-02-21 21:55:57 +0000

    added navbar

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,7 +3,7 @@ import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 import Services from './Services';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 const App = () => {
   return (
     <div>
