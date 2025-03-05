09da2705df5cb002a92652ef9661ac668876ee04
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-03-05 02:40:56 +0000

    update: implement routing for ProfileDetails and ProfileSettings components

--- react-router-advanced/src/components/ProfileDetails.jsx
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 const ProfileDetails = () => {
   return (
     <div>
-      ProfileDetails
-      ProfileDetails
+      <Routes>
+        <Route path="/" element={<Profile />}>
+          <ProtectedRoute>
+            <Route path="/details" element={<ProfileDetails />} />
+          </ProtectedRoute>
+          <Route path="/details" element={<ProfileDetails />} />
+          <Route path="/settings" element={<ProfileSettings />} />
+        </Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileDetails
+export default ProfileDetails;
