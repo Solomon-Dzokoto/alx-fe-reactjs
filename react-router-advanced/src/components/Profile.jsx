b901901b69cff5481357b83f53f9c2b49239f49d
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-03-07 00:20:50 +0000

    update: modify the profile page

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 
-const  Profile = () => {
+const Profile = () => {
   return (
     <div>
-       Profile
-       Profile
+      <Routes>
+        <ProtectedRoute>
+          <Route path="/" element={<Profile />}>
+            <Route path="/details" element={<ProfileDetails />} />
+            <Route path="/settings" element={<ProfileSettings />} />
+          </Route>
+        </ProtectedRoute>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default  Profile
+export default Profile;
--- react-router-advanced/src/components/ProfileDetails.jsx
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -1,20 +1,8 @@
-import React from "react";
-import ProfileDetails from "./ProfileDetails";
-import ProfileSettings from "./ProfileSettings";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ProtectedRoute from "./ProtectedRoute";
+
 const ProfileDetails = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Profile />}>
-          <ProtectedRoute>
-            <Route path="/details" element={<ProfileDetails />} />
-          </ProtectedRoute>
-          <Route path="/details" element={<ProfileDetails />} />
-          <Route path="/settings" element={<ProfileSettings />} />
-        </Route>
-      </Routes>
+    ProfileDetails
     </div>
   );
 };
