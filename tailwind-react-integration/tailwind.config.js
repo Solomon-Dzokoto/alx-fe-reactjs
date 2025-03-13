c42a55322e93424e034578b12a2d2b9f312f8282
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-03-13 10:50:16 +0000

    feat:setup tailwindcss configuration

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*/.{js,ts,jsx,tsx}",
+    './index.html',
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
