36a9a7f59d1168ff72242a3030dc92fb501f5354
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-03-13 10:53:51 +0000

    feat:setup tailwindcss configuration

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,13 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/**/*/.{js,ts,jsx,tsx}",
-    './index.html',
-    "./public/**/*.html"
-  ],
-  theme: {
-    extend: {},
-  },
+   module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
   plugins: [],
-}
-
+};
